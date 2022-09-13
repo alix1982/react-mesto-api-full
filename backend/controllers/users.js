5ef6f10192732c449b719c8c068da52b8151e520
Alix <alix1982@yandex.ru>
2022-09-13 18:49:13 +0300

    Проектная работа 4.15c.27

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -136,7 +136,7 @@ module.exports.login = (req, res, next) => {
           if (!matched) {
             throw new NoAuthErr('Неправильные почта или пароль');
           }
-          const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+          const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
           // const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
           res.send({ token, message: 'Всё верно!' });
         });
